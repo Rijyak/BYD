df378c9f2e6bcdc1d5915c140d91cbe5ee877be8
Rijyak <bajracharyarijyak@gmail.com>
2020-06-09 10:14:28 +0545

    hello

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BYD</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
